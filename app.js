a245c65c02a075e22fd28e6cae0a7e1541ed5779
paul crisan <crisanpaul@gmail.com>
2020-10-24 22:22:56 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var sassMiddleware = require("node-sass-middleware");
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+var app = express();
+
+app.use(connectLivereload());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
